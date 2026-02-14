faedf17bb01c91c306b53c2806dd10a4ba643740
mukherjeerani2606 <mukherjeerani456@gmail.com>
2026-02-14 23:25:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
-// THEME TOGGLE
-const toggle = document.getElementById("themeToggle");
+const btn = document.getElementById("themeToggle");
 
-if(toggle){
-    toggle.addEventListener("click", () => {
-        document.body.classList.toggle("light");
-
-        if(document.body.classList.contains("light")){
-            toggle.textContent = "☀️ Light mode";
-        } else {
-            toggle.textContent = "🌙 Dark mode";
-        }
-    });
-}
-
-// ABOUT PANEL
-const aboutBtn = document.getElementById("aboutBtn");
-const aboutPanel = document.getElementById("aboutPanel");
-const closeAbout = document.getElementById("closeAbout");
-
-if(aboutBtn){
-    aboutBtn.addEventListener("click", () => {
-        aboutPanel.classList.add("active");
-    });
-}
-
-if(closeAbout){
-    closeAbout.addEventListener("click", () => {
-        aboutPanel.classList.remove("active");
-    });
-}
+btn.onclick = () => {
+  document.body.classList.toggle("light");
+};
