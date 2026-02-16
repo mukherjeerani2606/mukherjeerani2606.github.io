7933cabf582d02fe99e667b256c874309c445247
mukherjeerani2606 <mukherjeerani456@gmail.com>
2026-02-16 18:35:10 +0530

    Create poem1.js

---
+++ poem1.js
@@ -0,0 +1,16 @@
+poems.push({
+  id: "poem1",
+  title_bn: "নববর্ষ",
+  title_en: "New Year",
+  desc_bn: "অপ্রিয় সত্য কথা ...",
+  desc_en: "The bitter truth ...",
+  text_bn: `বছরের প্রথমদিনে প্রণাম আসে যায় গুনে গুনে...
+  
+  বলে প্রনাম নিও, ও বলে আশীষ দিও!
+  ...`,
+  text_en: `On the first day of the year...
+  
+  Say your regards, and give your blessings!
+  ...`
+});
+buildUI();
