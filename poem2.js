5c139e2473a518c74dc6af472dee70891d3ac864
mukherjeerani2606 <mukherjeerani456@gmail.com>
2026-02-16 18:35:46 +0530

    Create poem2.js

---
+++ poem2.js
@@ -0,0 +1,36 @@
+poems.push({
+  id: "poem2",
+  title_bn: "স্বার্থ",
+  title_en: "Selfishness",
+  desc_bn: "মানুষের স্বার্থপরতার বাস্তবতা...",
+  desc_en: "Reality of selfishness...",
+  text_bn: `দুনিয়াটা স্বার্থে ভরা 
+স্বার্থ ছাড়া সবই মরা !
+বন্ধু হোক সে যতই আপন 
+ভালোবাসাতে সবাই কূপন
+
+নিজের স্বার্থ ফুরালে বুঝি 
+কেউ কাউকে না চিনি 
+যতই ভাবি আাপন সবাই 
+ততই বেশি ঠকায় সবাই
+
+হাসলে আমি থাকবে সে 
+কাঁদলে পরে 'তুমি' কে ?? 
+এ জগতের কেউ কারো নয় 
+মনে পড়ে তার , যখন পার হয় না সময়`,
+  text_en: `The world is full of selfishness
+Without selfishness, everything dies!
+No matter how close a friend is
+Everyone cheats even in love.
+
+When one's own interest ends
+No one really knows anyone
+The more I think everyone is close
+The more everyone deceives.
+
+If I smile, he/she stays
+If I cry, who cares??
+No one belongs to anyone in this world
+They remember only when time is scarce`
+});
+buildUI();
