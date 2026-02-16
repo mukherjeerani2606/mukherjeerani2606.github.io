cfd332a91704420294cf2d63ae8aca81ac0f8306
mukherjeerani2606 <mukherjeerani456@gmail.com>
2026-02-16 21:24:26 +0530

    Update poem1.js

--- poem1.js
+++ poem1.js
@@ -1,7 +1,7 @@
 poems.push({
   id: "poem1",
-  title_bn: "নববর্ষ",
-  title_en: "New Year",
+  title_bn: "নববর্ষ | কবিতা",
+  title_en: "New Year | Poem",
   desc_bn: "অপ্রিয় সত্য কথা ...",
   desc_en: "The bitter truth ...",
   text_bn: `বছরের প্রথমদিনে প্রণাম আসে যায় গুনে গুনে 
