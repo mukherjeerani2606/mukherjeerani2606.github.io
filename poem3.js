e262ada5445b15771450bb4527587cf747a5e931
mukherjeerani2606 <mukherjeerani456@gmail.com>
2026-03-20 19:46:12 +0530

    Create poem3.js

---
+++ poem3.js
@@ -0,0 +1,44 @@
+poems.push({
+  id: "poem1",
+  title_bn: "কল্পনা  | কবিতা",
+  title_en: "কল্পনা  | Poem",
+  pageTitle_bn: "নববর্ষ",
+  pageTitle_en: "কল্পনা ",
+  desc_bn: "কিছু কল্পনার কথা  ...",
+  desc_en: "Some Thoughts on Imagination ...",
+  text_bn: `শোনো হে প্রেমিক,
+ওই যে দূরে শীতের দুপুরে দাঁড়িয়ে তুমি তোমার ছাদে,
+আমি আমার জানালার পাশে এলিয়ে আছি আধো ঘুমে,
+ক্লান্ত দুই চোখ বন্ধ,
+তবুও যে টের পাই তোমার ও রোদের স্নিগ্ধ আলো।
+
+স্বপ্নে দেখি পড়ছ তুমি এক উপন্যাস আমাকে মনে করে,
+পেলে কি খুঁজে আমায়, কোনো অধ্যায়ের নারী চরিত্রে?
+	নাকি তুমি উপসংহারের মুখে?
+
+ঘুম ভেঙে দেখি তুমি উপন্যাস ছেড়ে তোমার ছাদবাগানে নিমগ্ন,
+আর তোমার ওই পোষা পাখি,
+আজ আমার ঘুম ভাঙার দায়ে দায়ী।
+তবু সে ছাড় পেল কেন জানো?
+হেতু সে নিয়ে হাজির তোমার দেওয়া প্রেম দিবসের চিঠি ও তোমার ছাদবাগানের ফুল সমগ্র।`,
+  text_en: `Listen, my beloved,
+There you stand afar, in the hush of a winter noon, upon your rooftop,
+While I rest by my window, drifting in a half-woven sleep,
+My weary eyes closed—
+Yet I still feel the tender grace of your sunlit warmth.
+
+In my dreams, I find you turning the pages of a novel, thinking of me—
+Tell me, did you find me there,
+In some woman hidden within its chapters?
+Or had you already wandered to the quiet of its end?
+
+I awaken to find you’ve set the novel aside,
+Now lost within your rooftop garden,
+And that little bird of yours—
+She, your quiet messenger, broke my sleep today.
+Yet, do you know why she was forgiven?
+
+For she came as a bearer of your heart,
+Carrying your love-soaked letter and the tender blooms from your rooftop garden.`
+});
+buildUI();
